20328d8e715ea465af187052e3f42fe19008b7ef
chris <chrishethertonweb@gmail.com>
2022-07-18 13:16:35 +0200

    4 ex done

--- sum.js
+++ sum.js
@@ -1,6 +1,12 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
 
-const sum = () => {};
+const sum = (...values) => {
+  let sumUp = 0;
+  for (i = 0; i < values.length; i++) {
+    sumUp += values[i];
+  }
+  return sumUp;
+};
 
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
